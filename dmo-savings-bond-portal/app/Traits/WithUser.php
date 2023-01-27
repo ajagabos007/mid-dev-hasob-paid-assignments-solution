8b68a53bfc4a845e44918e1d9678d96516297367
ajagabos007 <ajagabos007@gmail.com>
2023-01-27 07:45:44 +0100

    rename testing db

---
+++ dmo-savings-bond-portal/app/Traits/WithUser.php
@@ -0,0 +1,39 @@
+<?php 
+
+namespace App\Traits;
+use Hasob\FoundationCore\Models\User;
+
+trait WithUser {
+    /**
+     * The instance of a user
+     * @var Hasob\FoundationCore\Models\User;
+     */
+    protected $user;
+
+    /**
+     * The instance of a userAuth
+     * @var Hasob\FoundationCore\Models\User;
+     */
+    protected $userAuth;
+
+     /**
+     * The instance of a admin
+     * @var Hasob\FoundationCore\Models\User;
+     */
+    protected $admin;
+
+    /**
+     * The instance of a adminAuth
+     * @var Hasob\FoundationCore\Models\User;
+     */
+    protected $adminAuth;
+
+    /**
+     * 
+     */
+    public function setUpUser(){
+
+    }   
+
+    
+}
